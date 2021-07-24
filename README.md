9005d52bedd6243a6317b53e75747fc093a22a69
Metalistenia <sauceda92zuki@gmail.com>
2021-07-24 15:40:58 -0500

    committ

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Quiz
+# Code-Quiz
+
+
+<h1>Description</h1>
+
+The purpose of this assignment was to create a timer-based coding assessment using vanilla JavaScript that behaves like so:
+
+The application has a few multiple choice questions and the ability to store top scores on the user's device. The number of questions decides the length of time for the quiz (15 seconds/question) once the user clicks "Start", but they are penalized 10 seconds if they answer a question incorrectly. The final score is based on the amount remaining on the timer.
+
+At the end of the quiz (when the user answers all questions or time is up), upon viewing their score, the user is able to enter their initials to add their score to the High Scores list. Once they enter their initials, the scoreboard will appear with an option to clear the board or restart the quiz.
+
---
+++ scrypt.js
@@ -0,0 +1,151 @@
+var quizQuestions = document.getElementById("quiz-questions");
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+var timecounter = document.getElementById("timecounter");
+var titleitem = document.getElementById("title-item");
+var nextQuestions 
+var questionanswers = document.getElementById("question-answers");
+var myScore = document.getElementById("score");
+var btnScore = document.getElementById("btnScore");
+var currentindex = 0;
+var score = 0;
+var count = 75;
+var alert =document.getElementById("alert");
+var info = document.getElementById("info");
+// var addscore = document.getElementById("addscore");
+// var submitresult = document.getElementById("submitresult");
+var allScores = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var questions = [
+    {
+        title: "Capital of Spain",
+        choices: ["Sevilla","Bilbao","Madrid", "Barcelona"],
+        answer : "Madrid"    
+    },
+    {
+        title: "Capital of Mexico",
+        choices: ["Mexico City","Monterrey","Guadalajara", "Puebla"],
+        answer : "Mexico City"    
+    },
+    {
+        title: "Capital of Italy",
+        choices: ["Milan","Florence","Rome", "Venice"],
+        answer : "Rome"    
+    },
+    {
+        title: "Capital of Argentina",
+        choices: ["Rosario","Buenos Aires","Mendoza","La Plata"],
+        answer : "Buenos Aires"    
+    },
+    {
+        title: "Capital of USA",
+        choices: ["Boston","Chicago","New York", "Washington D.C"],
+        answer : "Washington D.C"    
+    },
+]
+btnStart.addEventListener("click", starQuiz);
+function starQuiz(){
+    if(storedScores !==null) {
+        allScores = storedScores;
+    }
+    info.classList.add("d-none")
+    btnStart.classList.add("d-none")
+    timecounter.classList.remove("d-none")
+    quizQuestions.classList.remove("d-none")
+    nextQuestions= questions[currentindex]
+    console.log(nextQuestions.title)
+    
+        displayQuestion(nextQuestions)
+
+    gametime()
+}
+btnScore.addEventListener("click" , function(){
+    let name = document.getElementById("inputScore").value
+    scorePage(name, count)
+});
+// Time set
+
+function gametime(){
+
+    var timeinterval = setInterval(function(){
+        timer.innerText = count
+         count--;
+        }, 1000);
+
+}
+
+function scorePage(a, b) {
+
+    var userData = {
+        inits: a,
+        userScore: b
+    };
+    allScores.push(userData);
+
+    localStorage.setItem("userData", JSON.stringify(allScores));
+    location.href = "score.html";
+}
+
+function displayQuestion(question){
+    titleitem.innerText=question.title
+    question.choices.forEach(element => {
+     var button =document.createElement("button")
+    button.className="btn-primary btn-block text-left"
+    button.innerText=element
+    // questionanswers.innerHTML=""
+    questionanswers.appendChild(button)
+    button.addEventListener("click", displaynextQuestion)
+    });
+}
+
+
+function displaynextQuestion(e){
+    currentindex++
+    if(currentindex < questions.length){
+        correction(e.target.innerText == nextQuestions.answer)
+        questionanswers.innerHTML=""
+        if(currentindex < questions.length){    
+            nextQuestions= questions[currentindex]
+            displayQuestion(nextQuestions)  
+        }else {
+            currentindex = 0
+            displayQuestion(nextQuestions)  
+        }
+
+    }else{
+        console.log("endgame")
+        endgame()
+        
+
+    }
+    
+     
+}
+function correction(response){
+    
+    if(response){
+        alert.innerText= "Good"
+        console.log("Good")
+    }else {
+        alert.innerText="Wrong"
+        count = count -15
+        timer.innerHTML = count
+        console.log("Wrong")
+
+    }
+    setTimeout(function(){
+        alert.innerText=""
+    
+        }, 1000);
+
+}
+ function endgame (){
+    // btnStart.classList.add("d-none")
+    myScore.innaText = count
+    addscore.classList.remove("d-none")
+    timecounter.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+    addscore.classList.remove("d-none")
+
+
+ }
